7338dc9676eff4a4b70952bc1229e4105fd04ec9
TheMaskedPony <Leponeymasque22@gmail.com>
2021-12-22 10:39:38 +0100

    Nettoyage du main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Tripadvisor comments");
-
-        String filepath = "C:\\Users\\Travail\\IdeaProjects\\SDA-21-22\\SDA_Serie12_TextMining\\data\\tripadvisor_hotel_reviews.csv";
-
-        ICommentsAnalyzer analyzer = new CommentsAnalyzer();
-        analyzer.loadComments(filepath);
-
-        for(Comment comment : analyzer.getComments())
-            System.out.println(comment.toString());
-
     }
 
 }
